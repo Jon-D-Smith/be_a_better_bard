be7aa702ff7dfbb8f391d1dd7cc0c38f9847b502
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-04 21:40:54 -0500

    Added Navbar and started on Character component

---
+++ src/components/Characters.js
@@ -0,0 +1,15 @@
+const Characters = () => {
+    return ( 
+        <div className="characters">
+            <header>
+                Characters
+            </header>
+            <div className="characterTab">
+                <img src="" />
+                <h3>Butters</h3>
+            </div>
+        </div>
+     );
+}
+ 
+export default Characters;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div className="navigation">
+            <a className="logo" href="#">Be A Better Bard</a>
+            <a className="menu"href="#">Home</a>
+            <a className="menu" href="#">All Spells</a>
+            <a className="account menu" href="#">Profile</a>
+        </div> 
+       
+     );
+}
+ 
+export default Navbar;
