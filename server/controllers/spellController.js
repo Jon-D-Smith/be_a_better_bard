1976433cef85ac1de350c0e0867297c51fb996f8
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-25 14:23:50 -0500

    Added node-postgress; setup basic routes and queries

--- server/controllers/spellController.js
+++ server/controllers/spellController.js
@@ -1,14 +1,43 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 module.exports = {
 
-  // gets all of spell lists ber character id
-  getSpellListById: (req, res) => {
-    const { character_id } = req.body;
-    req.app
-      .get("db")
-      .chat.get_spell_list_by_id(req_id)
-      .then(response => {
-        res.status(200).json(response);
-      })
-      .catch(err => console.log(err))
+
+  getSpellListById:  async (req,res) => {
+    try{
+      const client = await pool.connect();
+      const result = await client.query(`
+      SELECT sl.*, s.* 
+      FROM SPELL_LIST sl 
+      INNER JOIN SPELLS s ON sl.spell_id = s.spell_id
+      WHERE character_id = ${1} order by spell_list_id`);
+
+      const results = { 'results': (result) ? result.rows : null};
+      res.json( results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.json("Error " + err);
+    }
+  }
+  ,
+
+  getSpellList: async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM spells');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json( results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.json("Error " + err);
+    }
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,45 +1,19 @@
 require("dotenv").config();
-
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const massive = require("massive");
-const { json } = require("body-parser");
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3001;
 const app = express();
 
-const server = require('http').createServer(app);
+const { getSpellListById, getSpellList } = require('./controllers/spellController');
 
-const { getSpellListById } = require('./controllers/spellController');
+app.use(express.json());
 
-app.use(json());
-
-app.use(express.static(`${__dirname}/../build`));
-
-massive(process.env.CONNECTION_STRING)
-  .then(db => {
-    app.set("db", db);
-    console.log("Database Connected");
-  })
-  .catch(err => console.log(err));
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24 * 7
-//     }
-//   })
-// );
+// app.use(express.static(`${__dirname}/../build`));
 
 //SPELLS ENDPOINT
-app.post("/characters/:id", getSpellListById);
+app.get("/spells", getSpellList);
+app.get("/characters/:id", getSpellListById);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
-
-server.listen(port, console.log(`Listening on ${port}`));
+app.listen(port, console.log(`Listening on ${port}`));
